68e1eefb64d5863c692d325c6602944bc00d7986
Adrián Villamayor <adrian.villamayor@gmail.com>
2024-10-15 19:26:36 +0200

    Actualizar configuración de ESLint y añadir reglas de linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,23 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
+
 export default [
-  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+        globals: globals.browser
+    },
+    env: {
+      node: true,
+      es6: true  
+    },
+    rules: {
+      'no-unused-vars': 'error',
+      'no-console': 'warn',
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'double'],
+    },
+  },
 ];
